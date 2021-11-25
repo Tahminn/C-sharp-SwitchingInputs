eabaef1ce1228a4b5c4f4a49fdbe9b2136cc1d6a
Tahmin Fatiyev <tahminshf@code.edu.az>
2021-11-26 02:01:18 +0400

    Switching inputs without using third input

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Switching_Inputs
+{
+    class Program
+    {
+        public static int answerSubtract { get; private set; }
+        public static object MessageBox { get; private set; }
+
+        static void Main(string[] args)
+        {
+            int num1 = 4;
+            int num2 = 6;
+
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            num1 = num1 - num2;
+
+            Console.Write(num1 + " " + num2);
+        }
+    }
+}
